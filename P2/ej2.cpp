0c3b31c459f278d93c4353bdc3a333449a853a4f
Samuel-fps <samuel.fernandezpeinado@gmail.com>
2023-11-12 21:02:08 +0100

    P2 Hechos ejercicio 1, 2 y medio del 3

--- P2/ej2.cpp
+++ P2/ej2.cpp
@@ -10,49 +10,39 @@
 
 using namespace std;
 
-bool prueba_ordena(int);
+bool prueba_ordena(int v[], int o[], unsigned n, int nAlgoritmo);
 
 int main(){
-    if(prueba_ordena(2))
-        std::cout << "El orden por intercambio es correcto." << std::endl;
-    else
-        std::cout << "El orden por intercambio NO es correcto." << std::endl;
-    return 0;
-}
-
-bool prueba_ordena(int nAlgoritmo){
     int v[N];
     int o[N];
+    int nAlgoritmo=3;
 
     // Generar el vector
-    for(int tam = 1 ; tam <= N ; tam++){
-        v[tam-1] = tam-1;
-        std::memcpy(o, v, sizeof v);
+    for(int n=1 ; n <= N ; n++){
+        v[n-1] = n - 1;
+        memcpy(o, v, sizeof v);
+        
         // Generar todas las permutaciones
         do {
-            switch(nAlgoritmo){ // Llamar al algoritmo elegido
-                case 1: ordenacion_intercambio(v, v+tam);   break;
-                case 2: cout << "Probando seleccion..."   << endl; ordenacion_seleccion(v, v+tam);     break;
-                case 3: cout << "Probando insercion..."   << endl; ordenacion_insercion(v, v+tam);     break;
-                default: std::cout << "El numero de algoritmo no es valido (numeros validos 1, 2, 3)";
-            }
-
-            std::cout << "[";
-            for(int i = 0 ; i < tam ; i++)
-                std::cout << o[i] << " ";
-            std::cout << "]" << std::endl;
-
-            std::cout << "[";
-            for(int i = 0 ; i < tam ; i++)
-                std::cout << v[i] << " ";
-            std::cout << "]" << tam << std::endl;
-            //std::cout << std::memcmp(v, o, tam) << std::endl;
-
-            if(memcmp(v, o, tam)) // Comprobar si está bien ordenada
-                return false; // no son iguales
-        } while(next_permutation(v, v+tam));
+            if(!prueba_ordena(v, o, n, nAlgoritmo)){} // Comprobar si está bien ordenada
+                //return 1; // no son iguales
+        } while(std::next_permutation(v, v+n));
     }
-    return true;
+    cout << "Funciona correctamente." << endl;
+}
+
+bool prueba_ordena(int v[], int o[], unsigned n, int nAlgoritmo){
+    int copia[N];
+
+    for(unsigned i=0 ; i < n ; i++)
+        copia[i] = v[i];
+    switch(nAlgoritmo){ // Llamar al algoritmo elegido
+        case 1: ordenacion_intercambio(copia, copia+n);   break;
+        case 2: ordenacion_seleccion(copia, copia+n);     break;
+        case 3: ordenacion_insercion(copia, copia+n);     break;
+        default: std::cout << "El numero de algoritmo no es valido (numeros validos 1, 2, 3)";
+    }
+    return (memcmp(copia, o, n) == 0);
 }
 
 /*
--- P2/ej3.cpp
+++ P2/ej3.cpp
@@ -3,30 +3,37 @@
 
 #include <iostream>
 #include <algorithm>
+#include <random>
 
 #define N 20000 // Longitud maxia del ejemplar
 #define I 1000 // Incremento
+
+using namespace std;
 
 int main() {
     int v[N], veces=0;
     double t1=0;
     cronometro c;
 
+    // Generaor para el shuffle
+    random_device rd;
+    mt19937 gen(rd());
+
     // generar ejemplares de rueba
-    for(int i = 1000 ; i < N ; i += 1000){
+    for(int i=1000 ; i < N ; i += 1000){
         for(int j = 0 ; j < i ; j++)
             v[j] = j+1;
         veces++;
 
-        std::random_shuffle(0, i);
+        shuffle(v, v+i, gen);
 
         c.activar();
-            ordenacion_intercambio(0, i);
+            ordenacion_intercambio(v, v+i);
         c.parar();
         t1 += c.tiempo();
     }
-
+    cout << veces << " Tiempo: " << t1 << endl; 
     t1 = t1/veces; // divido el tiempo entre las veces que se ha ejecutado
 
-    std::cout << "Intercambio tarda " << t1 << "segundos en el caso promedio." << endl;
+    std::cout << "Intercambio tarda " << t1 << " segundos en el caso promedio." << endl;
 }
