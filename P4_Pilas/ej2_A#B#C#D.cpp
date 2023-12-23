578623c50d98adf4136595b210f90f206f1da640
Samuel-fps <samuel.fernandezpeinado@gmail.com>
2023-12-23 10:38:47 +0100

    ej2 terminado

--- P4_Pilas/ej2_A#B#C#D.cpp
+++ P4_Pilas/ej2_A#B#C#D.cpp
@@ -1,5 +1,25 @@
 /*  EJERCICIO 2
     Escriba una función que determine y devuelva si una secuencia de caracteres de entrada
-    es de la forma A#B#C#D . . ., donde A, B, C, D,. . . son de la forma X&Y , que a su vez
+    es de la forma A#B#C#D..., donde A, B, C, D,... son de la forma X&Y , que a su vez
     están separados por el caráter ‘#’.
 */
+#include <string>
+#include "pilaEnla.h"
+#include "ej1_cadena&inversa.h"
+
+bool cadenasInvertidas(char cad[]){
+    char* aux = new char[1];
+
+    for(size_t i = 0; i < strlen(cad) ; i++){
+        if(cad[i] != '#')
+            aux[i] = cad[i];
+        else{
+            if(!XandY(aux))
+                return false;
+            delete[] aux;
+            aux = new char[1];  
+        }
+    }
+    delete[] aux;
+    return true;
+}
